49563b78a36b3067b72142823e2397011fcdc3ee
itztla <hanumanman37@gmail.com>
2025-01-06 23:22:43 +0700

    chore: rename

--- treePathSum/main_test.ts
+++ treePathSum/main_test.ts
@@ -4,7 +4,7 @@ import { arrayToTreeNode, hasPathSum } from "./main.ts";
 const root = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1];
 const targetSum = 22;
 
-Deno.test(function treeTest() {
+Deno.test(function testTree() {
   const treeNode = arrayToTreeNode(root);
   assertEquals(hasPathSum(treeNode, targetSum), true);
 });
